9ae4ba87ce6e53b32b829a1aad8a692cb7759f33
Villy Devlioti <vadevlio@gmail.com>
2019-10-19 10:39:15 -0400

    most functionality done

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
     var topics = ["cat", "dog", "otter", "kitten", "puppy", "elephant"];
 
+    //rendering my buttons
     function renderButtons(){
         $("#buttons-view").empty();
         $.each(topics, function(index, value){
@@ -11,50 +12,43 @@ $(document).ready(function() {
             console.log(topics);
     }
 
+    //function to display gifs
     function displayGifs (obj) {
         var gifQuery = $(this).attr("data-name");
         console.log(gifQuery);
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q="+gifQuery+"&limit=10";
         console.log(queryURL); 
+        $("#gifs-view").empty();
 
-    // Creates AJAX call for the specific gif button being clicked
+        // Creates AJAX call for the specific gif button being clicked
         $.ajax({
         url: queryURL,
         method: "GET"
-        }).then(function(response) {
-        console.log(response);
-        $("#gifs-view").empty();
-        $("#gifs-view").append($("<img>").attr("src",response.data.images.downsized.url));
+        })
+        
+        .then(function(response) {
+            console.log(response);
+            console.log(response.data);
+            $.each(response.data, function(index,value) {
+                console.log(index,value, value.images.fixed_height.url);
+                $("#gifs-view").append($("<img>").attr("src",value.images.original_still.url));
+            });
         });
 
     }
-
-
-
+    
+    //this handles the submit button
     $("#add-gif").on("click", function(e) {
         e.preventDefault(); //prevent re-initializing the screen! It happens when we have a submit button
         var newButton = $("#gif-input").val(); // or $("#gif-input").val().trim(); to get rid of the spaces in between
-        //console.log(newButton+",");
         topics.push(newButton);
         console.log(topics);
         renderButtons();
     });
 
-/*     $(".gif-preset").on("click", function() {
-        var gifQuery = $(this).attr("data-name");
-        console.log(gifQuery);
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q="+gifQuery+"&limit=10";
+    //calls the display function, each time a button is clicked
+    $(document).on("click", "button", displayGifs);
 
-    // Creates AJAX call for the specific gif button being clicked
-        $.ajax({
-        url: queryURL,
-        method: "GET"
-        }).then(function(response) {
-        console.log(response);
-        $("#gifs-view").empty();
-        response.data.forEach(displayGifs);
-    });  */
-
-    $(document).on("click", ".gif-preset", displayGifs);
+    //this pretty much starts it all
     renderButtons();
 });
